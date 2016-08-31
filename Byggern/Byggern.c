90aadd245aea9dedb81d8638d8f20109cd863582
unknown <audunel@ITK-D653.win.ntnu.no>
2016-08-31 14:57:41 +0200

    ...

--- Byggern/Byggern.c
+++ Byggern/Byggern.c
@@ -18,6 +18,31 @@
 #define BitFlip(p,m)((p)^=(m))
 #define Bit(x)(0x01<<(x))
 
+#define FOSC 4900000
+#define BAUD 9600
+#define MYUBRR FOSC/16/BAUD-1
+
+void initUSART(unsigned int ubrr){
+	/* Set Baud rate */ 
+	UBRRH = (unsigned char)(ubrr >> 8);
+	UBRRL = (unsigned char)ubrr;
+	/* Enable receiver and transmitter*/
+	USCRB = (1 << RXEN)|(1 << TXEN)
+	/* Set frame format: 8data, 2stop bit */
+	UCSRC = (1 << URSEL)|(1 << USBS)|(3 << UCSZ0)
+}
+
+void transmit_USART(unsigned int data){
+	while ( !( USCRA & (1 << UDRE))){}
+	UDR = data;
+}
+
+void receive_USART(unsigned int data){
+	
+}
+
+
+
 int main(void)
 {
 	BitSet(DDRC,Bit(0));
